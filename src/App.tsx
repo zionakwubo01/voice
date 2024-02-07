9ba32207fd4283160949b72fc96501b9eb89b5bb
zionakwubo01 <zionakwubo@gmail.com>
2024-02-07 08:23:18 +0100

    update3

--- src/App.tsx
+++ src/App.tsx
@@ -15,23 +15,23 @@ const SpeechRecognitionComponent: React.FC = () => {
 
   return (
     <div className="w-full h-[100vh] flex items-center justify-center">
-      <div className="w-[40%] h-[30%] bg-gray-200">
+      <div className="w-[40%] h-[30%] bg-gray-200 flex flex-col items-center justify-between">
         <div className="font-bold text-[20px]"> {listening ? "on" : "of"}</div>
         <button
           onClick={startListening}
-          className="w-[200px] h-[40px] bg-blue-400"
+          className="w-[100px] h-[40px] bg-blue-400"
         >
           Start Listening
         </button>
         <button
           onClick={stopListening}
-          className="w-[200px] h-[40px] bg-blue-400"
+          className="w-[100px] h-[40px] bg-blue-400"
         >
           Stop Listening
         </button>
         <button
           onClick={resetTranscript}
-          className="w-[200px] h-[40px] bg-blue-400"
+          className="w-[100px] h-[40px] bg-blue-400"
         >
           Reset Transcript
         </button>
